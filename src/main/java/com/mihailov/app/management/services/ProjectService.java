2ceac6d99a27dc1463b67a30f2feed3bd790e365
mihailovadrian <adrianmihailov123@gmail.com>
2019-10-16 08:10:23 -0700

    Create Backlog and POST for Project task

--- src/main/java/com/mihailov/app/management/services/ProjectService.java
+++ src/main/java/com/mihailov/app/management/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.mihailov.app.management.services;
 
+import com.mihailov.app.management.domain.Backlog;
 import com.mihailov.app.management.domain.ProjectEntity;
 import com.mihailov.app.management.exceptions.ProjectIdException;
+import com.mihailov.app.management.repositories.BacklogRepository;
 import com.mihailov.app.management.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,21 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
-
+    @Autowired
+    private BacklogRepository backlogRepository;
     public ProjectEntity saveOrUpdate(ProjectEntity project) {
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            if (project.getId() == null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProjectEntity(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+            if(project.getId()!=null)
+            {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
             return projectRepository.save(project);
         } catch (Exception ex) {
             throw new ProjectIdException("Project ID " + project.getProjectIdentifier() + "  not good.");
---
+++ src/main/java/com/mihailov/app/management/web/ProjectTaskController.java
@@ -0,0 +1,31 @@
+package com.mihailov.app.management.web;
+
+import com.mihailov.app.management.domain.ProjectTask;
+import com.mihailov.app.management.services.ProjectTaskService;
+import com.mihailov.app.management.services.ValidationErrorMapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/backlog")
+@CrossOrigin
+public class ProjectTaskController {
+    @Autowired
+    private ProjectTaskService projectTaskService;
+    @Autowired
+    private ValidationErrorMapService errorMapService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
+        ResponseEntity<?> errorMap = errorMapService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
